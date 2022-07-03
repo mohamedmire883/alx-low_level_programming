0eedde9edff8930be9f895819f7159916fbf594c
mohamed ahmed <mohamedire883@gmail.com>
2022-07-03 10:46:23 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
-julien@ubuntu:~/c/0x00$ gcc 6-size.c -m32 -o size32 2> /tmp/32
-julien@ubuntu:~/c/0x00$ gcc 6-size.c -m64 -o size64 2> /tmp/64
-julien@ubuntu:~/c/0x00$ ./size32
-Size of a char: 1 byte(s)
-Size of an int: 4 byte(s)
-Size of a long int: 4 byte(s)
-Size of a long long int: 8 byte(s)
-Size of a float: 4 byte(s)
-julien@ubuntu:~/c/0x00$ ./size64
-Size of a char: 1 byte(s)
-Size of an int: 4 byte(s)
-Size of a long int: 8 byte(s)
-Size of a long long int: 8 byte(s)
-Size of a float: 4 byte(s)
-julien@ubuntu:~/c/0x00$ echo $?
-0
-julien@ubuntu:~/c/0x00$ 
+
+/**
+* main - prints the size of various types
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int a;
+long int b;
+long long int c;
+char d;
+float f;
+
+printf(“size of a char : % lu  byte(s)\n”, (unsigned long)sizeof(d));
+printf(“size of an int : % d byte(s)\n”, (unsigned long)sizeof(a));
+printf(“size of a long int : % lu byte(s)\n”, (unsigned long)sizeof(b));
+printf(“size of a long long int : % lu byte(s)\n”, (unsigned long)sizeof(c));
+printf(“size of a float : % lu byte(s)\n”, (unsigned long)sizeof(f));
+return (0);
+}
