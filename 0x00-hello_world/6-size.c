d9b6372f3090f19975fbc5578be19447d38dbb70
mohamed ahmed <mohamedire883@gmail.com>
2022-07-03 07:40:32 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
+/**
+*main – prints the  size of various types
+* return: 0 if exited properly, non-zero otherwise
+*/
+int main(void)
+{
+printf(“size of a char: %d byte(s)\n*”, sizeof(char));
+printf(“size of an int: %d byte(s)\n*”, sizeof(int));
+printf(“size of a long int: %d byte(s)\n*”, sizeof(long int));
+printf(“size of a long long int: %d byte(s)\n*”, sizeof(long long  int));
+printf(“size of a float : %d byte(s)\n*”, sizeof(float));
+return (0);
+}
 
-Size of a char: 1 byte(s)
-Size of an int: 4 byte(s)
-Size of a long int: 4 byte(s)
-Size of a long long int: 8 byte(s)
-Size of a float: 4 byte(s)
-julien@ubuntu:~/c/0x00$ ./size64
-Size of a char: 1 byte(s)
-Size of an int: 4 byte(s)
-Size of a long int: 8 byte(s)
-Size of a long long int: 8 byte(s)
-Size of a float: 4 byte(s)
-julien@ubuntu:~/c/0x00$ echo $?
-0
-julien@ubuntu:~/c/0x00$
