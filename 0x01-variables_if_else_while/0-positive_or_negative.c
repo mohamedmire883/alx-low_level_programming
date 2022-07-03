7b39a816539753639509f7b1831f4606fe024436
mohamed ahmed <mohamedire883@gmail.com>
2022-07-03 15:07:54 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main->assign a random number to the variable n each time it is executed
+*print the last digit of the number stored in the variable n
+* Return: always 0
+*/
+int main(void)
+{
+int n
+strand(time(0));
+n = rand() – Rand_MAX / 2;
+printf(“last digit of % d is “, n);
+if (n > 5)
+{
+printf(“greater than 5”);
+}
