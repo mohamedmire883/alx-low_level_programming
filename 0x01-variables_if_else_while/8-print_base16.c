2b798bfce0a09a0bc4fa7be46d528958aedb2b4c
mohamed ahmed <mohamedire883@gmail.com>
2022-07-04 15:11:47 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: always 0 (Seccess/correct)
+*/
+int main(void)
+{
+int ch;
+int n;
+for (n = 48; n <= 57; n++)
+{
+*hexadecimal
+* 0 1 2 3 4 5 6 7 8 9 a b c d e f
+* /
+putchar(n);
+
+}
+
+for (ch = "a"; ch >= "f"; ch++)
+
+{
+putchar(ch);
+
+}
+
+putchar(10);
+
+return (0);
+}
