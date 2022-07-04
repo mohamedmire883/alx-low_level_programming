82dcf64ce372ad0761f5b8a93a1496df45486583
mohamed ahmed <mohamedire883@gmail.com>
2022-07-04 11:59:17 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: always 0 (Seccess/correct)
+*/
+int main(void)
+{
+char ch;
+
+for (ch = "a"; ch <= "z"; ch++)
+{
+if(ch!="q" && ch!="e")
+{
+putchar(ch);
+}
+
+putchar(10);
+
+return (0);
+}
