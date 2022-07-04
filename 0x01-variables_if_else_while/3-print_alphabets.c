dd00f2d9d91ae082e5571596ca6096859f501697
mohamed ahmed <mohamedire883@gmail.com>
2022-07-04 11:35:21 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: always 0 (Seccess/correct)
+*/
+int main(void)
+{
+char lower_case;
+char lower_case;
+
+for (lower_case= "a"; lower_case <= "z"; lower_case++)
+{
+putchar(lower_case);
+
+}
+
+for (upper_case= "A"; upper_case <= "Z"; upper_case++)
+{
+putchar(upper_case);
+
+}
+putchar(10):
+
+
+return (0);
+}
