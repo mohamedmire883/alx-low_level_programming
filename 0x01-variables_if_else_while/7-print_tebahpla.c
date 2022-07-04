a5ba5669c9c96895a290ca35dec67bf71e65a3a1
mohamed ahmed <mohamedire883@gmail.com>
2022-07-04 14:28:35 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: always 0 (Seccess/correct)
+*/
+int main(void)
+{
+int ch;
+
+for (ch = "z"; ch >= "a"; ch--)
+
+{
+putchar(ch);
+
+}
+
+putchar(10);
+
+return (0);
+}
