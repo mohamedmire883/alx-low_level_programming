da6e2f7c27c9f171a0d59bf7b183bde5c1e62d30
mohamed ahmed <mohamedire883@gmail.com>
2022-07-04 14:10:34 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: always 0 (Seccess/correct)
+*/
+int main(void)
+{
+int ch;
+
+for (ch = 48; ch <= 57; ch++)
+
+{
+putchar(ch);
+
+}
+
+putchar(10);
+
+return (0);
+}
