5d774ee5d38fedcd807eb8e476c02ad18e9f86ab
mohamed ahmed <mohamedire883@gmail.com>
2022-07-04 18:19:06 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: always 0 (Seccess/correct)
+*/
+int main(void)
+{
+int ch;
+int n;
+int m;
+for (ch = 48; ch <= 57; ch++)
+{
+
+for (n = 49; n <= 57; n++)
+{
+for (m = 50 m <= 57; n++)
+{
+if (n > ch && m > n)
+{
+putchar(ch);
+putchar(n);
+putchar(m);
+if (ch != 55 || n != 56 || m != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+}
+putchar(10);
+return (0);
+}
