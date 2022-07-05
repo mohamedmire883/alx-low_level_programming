40a4ce844bf515eed13f59c87e90fe1314edb91c
mohamed ahmed <mohamedire883@gmail.com>
2022-07-05 16:37:40 -0700

    putcher printer

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
-#include <unistd.h>
+#include <main.h>
 
-/**
-* _putchar - writes the character c stdout
-* @c:the character to print
-*
-* Return: on success 1
-* on error , -1 is returned and errno is set appropriately,
-*/
-int _putchar(char c)
+**/ main - prints putchar
+* Return : always 0
+* /
+int main(void)
 {
-return (write(1, &c, 1));
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
+
 }
+
