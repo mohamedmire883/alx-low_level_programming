a5cfc3e10acad4d25d76e998dd1f53e48cd03091
mohamed ahmed <mohamedire883@gmail.com>
2022-07-04 17:37:20 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,12 +14,15 @@ for (ch = 48; ch <= 57; ch++)
 
 for (n = 49; n <= 57; ch++)
 {
+if(n > ch){
 putchar(ch);
 putchar(n);
 if (ch != 56 || n!= 57)
 {
 putchar(44);
 putchar(32);
+}
+}
 }
 }
 putchar(10);
