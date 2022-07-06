d27d726fa75e37b11c77cc6788d04065da792ac5
mohamed ahmed <mohamedire883@gmail.com>
2022-07-05 19:43:26 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
 * @c: a character to be checked
 * Return: returns1 and 0 depending on the condtion
 */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
