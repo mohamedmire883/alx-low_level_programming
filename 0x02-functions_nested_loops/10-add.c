0e41d5b89b2e1e6d957845c3b9c4de59b72cf55e
mohamed ahmed <mohamedire883@gmail.com>
2022-07-06 15:55:57 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,12 +2,13 @@
 
 /**
 * add -> adds two a and b
-* @a&&b: the passed args
+* @a: the passed args
+* @b: the passed args
 * Return: 0
 */
 int add(int a, int b)
 {
-int c = a + b;
-_putchar(c);
-return (c);
+
+_putchar(a + b);
+return (a + b);
 }
