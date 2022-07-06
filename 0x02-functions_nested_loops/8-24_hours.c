50a3980566ffdd0710c17b089dc0b0afe01b8e9b
mohamed ahmed <mohamedire883@gmail.com>
2022-07-06 12:58:09 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* void jack_bauer ->  prints 24 hours
+*/
+void jack_bauer(void)
+{
+int i, j;
+for (i = 0; i < 24; i++)
+{
+for (j = 0; j < 60; j++)
+{
+if (i < 10)
+{
+_putchar('0');
+_putchar(i + '0');
+}
+else if (i >= 10)
+{
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
+}
+if (j < 10)
+{
+_putchar(':');
+_putchar('0');
+_putcha(j + '0');
+}
+else if (j >= 10)
+{
+_putchar(':');
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
+}
+_putchar('\n');
+}
+}
