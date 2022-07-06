54dbbbce8f491232f93d2baa9c867515b20698e0
mohamed ahmed <mohamedire883@gmail.com>
2022-07-05 19:35:55 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* _isalpha - checks for alphapetically letters
+* @c: a character to be checked
+* Return: returns1 and 0 depending on the condtion
+*/
+int _isalpha(int c);
+{
+return ((c >= 'a' && c <= 'z')||(c >= 'A' && c <= 'Z'));
+}
