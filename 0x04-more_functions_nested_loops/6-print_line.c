4e9dfe7487f5c7a398d37a812f47f9a27687cad4
mohamed ahmed <mohamedire883@gmail.com>
2022-07-07 10:32:12 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_line -> printing line
+* @n: intiger params
+*/
+void print_line(int n)
+{
+int x;
+if (n <= 0)
+_putchar('\n');
+for (x = 0; x <= n; x++)
+_putchar('_');
+_putchar('\n');
+}
