01a7dc1825f19a51401945782efdbff42f156a1c
mohamed ahmed <mohamedire883@gmail.com>
2022-07-07 05:54:12 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* mul -> multiplying 2 numbers
+* @a: integer param1
+* @b: integer param2
+* Return: return success and intiger
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
