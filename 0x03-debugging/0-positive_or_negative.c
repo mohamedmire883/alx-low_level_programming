68fa23b1945d92d73f0ffa4806e79091e1e69fe5
mohamed ahmed <mohamedire883@gmail.com>
2022-07-07 17:19:01 -0700

    0-positive_or_negative.c

--- 0x03-debugging/0-positive_or_negative.c
+++ 0x03-debugging/0-positive_or_negative.c
@@ -12,7 +12,7 @@ int main(void)
 int n;
 
 srand(time(0));
-n = rand() - RAND_MAX / 2;
+n = i;
 if (n > 0)
 {
 printf("%d is positive\n", n);
@@ -25,4 +25,3 @@ printf("%d is negative\n", n);
 }
 return (0);
 }
-
