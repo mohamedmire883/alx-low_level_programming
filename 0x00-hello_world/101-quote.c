f7b7d152206422d070481e5217e685c61c5ff24b
mohamed ahmed <mohamedire883@gmail.com>
2022-07-09 13:50:33 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,7 @@
 * followed by a new line, to the standard error.
 *  Return: Always 0 (success)
 */
-int main(void)
+void main()
 {
 write("and that piece of art is useful\", - Dora Korpar, 2015-10-19\n");
-
-return (0);
 }
