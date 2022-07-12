4beb7cbe7590193ccb63d365b03d977f22b740ec
mohamed ahmed <mohamedire883@gmail.com>
2022-07-11 19:17:29 -0700

    task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,7 +11,7 @@ char tmp;
 int i, length1, length2;
 length1 = 0;
 length1 = 0;
-while (s[length] != '\0')
+while (s[length1] != '\0')
 length1++;
 length2 = length1 - 1;
 for (i = 0; i < length1 / 2; i++)
