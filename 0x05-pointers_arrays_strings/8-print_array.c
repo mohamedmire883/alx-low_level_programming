796248d77a56ef28ea0684dc61f53db11cd8ff49
mohamed ahmed <mohamedire883@gmail.com>
2022-07-12 07:20:28 -0700

    task8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* print_array -> prints number of arrays
+* @n: array parameter
+* @a: array parameter
+*/
+void print_array(int *a, int n)
+{
+a[6] = {4, 2, 3, 7, 8, 9};
+for (n = 0; n < 6; n++)
+{
+printf("%d ,", n);
+}
+printf("\n");
+}
