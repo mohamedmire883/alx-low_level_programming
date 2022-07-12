1cd65199267b5f6fec54241b93186edab1d932f1
mohamed ahmed <mohamedire883@gmail.com>
2022-07-11 19:38:27 -0700

    task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* puts2 -> puts 2 function
+* @str: str parameter
+*/
+void puts2(char *str);
+{
+int len;
+len = strlen(str);
+for (i = 0; i < length1 / 2; i++)
+_putchar(str[i]);
+_putchar('\n');
+}
+}
