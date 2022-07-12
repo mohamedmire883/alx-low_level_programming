8bebf56f04fce3327931dea6b42dbe83478b8819
mohamed ahmed <mohamedire883@gmail.com>
2022-07-12 10:44:14 -0700

    task8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,13 +11,13 @@ void print_array(int *a, int n)
 {
 int i[4];
 
-for (n = 0; n < 4; n++)
+for (n = 0; n <= 4; n++)
 {
 printf("%d ,", i[n]);
 n = *a;
-for (n = 0; n < 4; n++)
+for (n = 0; n <= 4; n++)
 {
-printf("%d ,", *a);
+printf("%d ,", a);
 a++;
 }
 }
