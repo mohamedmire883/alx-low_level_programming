143700d3bccafaceb32a831f2b7788480b616356
mohamed ahmed <mohamedire883@gmail.com>
2022-07-13 11:29:05 -0700

    task5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* string_toupper -> reversing an array
+* @x: string param;
+* Return: string
+*/
+char *string_toupper(char *)
+{
+int i = 0;
+while (x[i])
+{
+if (x[i] >= 97 && x[i] <= 122;)
+x[i] = x[i] - 32;
+i++;
+}
+return (x);
+}
