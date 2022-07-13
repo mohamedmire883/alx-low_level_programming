9ceaaedd4cf1bac2abaa98950dd0b9f3bf20c45a
mohamed ahmed <mohamedire883@gmail.com>
2022-07-13 09:05:23 -0700

    task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,5 @@
-include "main.h"
-include <string.h>
+#include "main.h"
+#include <string.h>
 
 /**
 * _strcat -> this is a function strcat
@@ -12,7 +12,7 @@ char *_strcat(char *dest, char *src)
 int len = 0, i;
 while (dest[len])
 len++;
-for (i = 0; src[i] != 0; i++)
+for (i = 0; src[i] != i0; i++)
 {
 dest[len] = src[i];
 len += 1;
