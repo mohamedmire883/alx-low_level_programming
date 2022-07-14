2251dcc2ebb5e87271fc5832d9fd290dbf4735bd
mohamed ahmed <mohamedire883@gmail.com>
2022-07-14 14:44:28 -0700

    task4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <string.h>
 /**
 * reverse_array -> reversing an array
 * @a: array a
@@ -10,16 +10,13 @@ void reverse_array(int *a, int n)
 n[9};
 a = 0;
 
-for (int i = 0; i < (length / 2); i++)
+for (int i = 0; i < (9 / 2); i++)
 {
-int length = 9;
 a = n[i];
 n[i] = n[length - i - 1];
 n[length - i - 1] = a;
 }
-for (int i = 0; i < length; i++)
-{
-int length = 9;
+for (int i = 0; i < 9; i++)
+
 n[1];
-}
 }
