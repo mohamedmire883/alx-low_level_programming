c9e69e3f237324ebaa5470fce8a5f1e771e8450a
mohamed ahmed <mohamedire883@gmail.com>
2022-07-25 15:58:12 -0700

    task0

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strdup -> strig duplicator function
+ * @str: string to be duplicated
+ * Return: a tring pointer
+ */
+char *_strdup(char *str)
+{
+
+int i = 1;
+if (str == Null)
+return (NULL);
+
+while ((string[i])
+i++;
+s = (char *)malloc(i *sizeof(char) + 1);
+if (s == Null)
+return (NULL);
+while (j < i)
+{
+s[j] = str[j];
+j++;
+}
+s[j] = '0';
+
+return (s);
+}
