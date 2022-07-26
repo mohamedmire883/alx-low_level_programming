23059be3714bb182926dbebfce5e6df2b4fcb137
mohamed ahmed <mohamedire883@gmail.com>
2022-07-25 17:01:17 -0700

    task2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * str_concat -> strig concatination function
+ * @s1: string 1
+ * @s2: string 1
+ * Return: string 1 + string2;
+ */
+char *str_concat(char *s1, char *s2);
+{
+
+int i = 0, j = 0, l = 0, k = 0;
+char *s;
+if (s1 == Null)
+s1 = "";
+if (s2 == Null)
+s1 = "";
+
+while (s1[i])
+i++;
+while (s2[j])
+j++;
+l  = i + j;
+s = (char *)malloc(l *sizeof(char) + 1);
+j = 0;
+while (k < l)
+{
+if (k <= i)
+s[k] = s1[k];
+if (k > i)
+{
+s[k] = s1[j];
+j++;
+}
+k++;
+}
+s[k] = '\0';
+return (s);
+}
